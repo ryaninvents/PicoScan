750dbdb028175f3c3acbef28d749314eaa47906b
Ryan Muller <ryan@ramuller.com>
2013-01-23 15:04:35 -0500

    basic one-camera calibration works

--- main.cpp
+++ main.cpp
@@ -18,70 +18,75 @@ using namespace fr;
 
 int main(int, char**)
 {
-    VideoCapture cap(1); // open the default camera
-    if(!cap.isOpened())  // check if we succeeded
+    VideoCapture cap1(1);
+    if(!cap1.isOpened())
         return -1;
 
-    cap.set(CV_CAP_PROP_FRAME_WIDTH,640);
-    cap.set(CV_CAP_PROP_FRAME_HEIGHT,480);
-    Mat edges;
-    Size size(12,9);
+    unsigned int i;
 
-    int counter = 10;
+    cap1.set(CV_CAP_PROP_FRAME_WIDTH,800);
+    cap1.set(CV_CAP_PROP_FRAME_HEIGHT,600);
+    Mat edges;
+    Size size(4,11);
+
+    int counter = 12;
 
     vector<Point2f> corners;
     bool found;
 
-    vector<Point3f> chess = fr::ChessGen::getBoard(size,1,false);
+    vector<Point3f> chess = fr::ChessGen::getBoard(size,1,true);
    /* for(unsigned int i=0;i<chess.size();i++){
         cout << chess[i].x << "," << chess[i].y << endl;
     }*/
 
     vector<vector<Point3f> > objectPoints;
-    vector<vector<Point2f> > imagePoints;
+    vector<vector<Point2f> > imagePoints1,imagePoints0;
 
     Mat camera = Mat::eye(3,3,CV_64F);
-    camera.at<double>(0,2) = 640/2;
-    camera.at<double>(1,2) = 480/2;
     Mat distortion = Mat::zeros(8, 1, CV_64F);
     vector<Mat > rvecs;
     vector<Mat > tvecs;
 
-    namedWindow("edges",1);
+    namedWindow("CAMERA 1",1);
+    found = false;
     for(;;)
     {
         Mat frame;
-        cap >> frame; // get a new frame from camera
+        cap1 >> frame; // get a new frame from camera
         cvtColor(frame, edges, CV_BGR2GRAY);
 
-        found = findChessboardCorners(edges,size,corners);
-        /*
-        found = findCirclesGrid(edges,size,corners
-                                ,CALIB_CB_ASYMMETRIC_GRID
-                                );//*/
-        if(found) frame.convertTo(edges,-1,0.2);
 
-        drawChessboardCorners(edges,size,corners,found);
+        //frame.convertTo(edges,-1,0.2);
 
-        imshow("edges", edges);
-        /*if(found){
-            //if(waitKey(200)>=0){
-            waitKey(300);
+        for(i=0;i<imagePoints1.size();i++)
+            drawChessboardCorners(frame,size,imagePoints1[i],true);
+
+        imshow("CAMERA 1", frame);
+        if(waitKey(200)>=0){
+            //found = findChessboardCorners(edges,size,corners);
+            //*
+            found = findCirclesGrid(edges,size,corners
+                                    ,CALIB_CB_ASYMMETRIC_GRID
+                                    );//*/
+            if(found){
                 objectPoints.push_back(chess);
-                imagePoints.push_back(corners);
+                imagePoints1.push_back(corners);
                 if(--counter<= 0)
                     break;
-            //}
+            }
         }
-        else waitKey(30);*/
-        if(waitKey(30)>1) break;
+        //else waitKey(30);
+        //if(waitKey(30)>1) break;
     }
 
-    //double rpe = calibrateCamera(objectPoints,imagePoints,Size(800,600),camera,distortion,rvecs,tvecs,CV_CALIB_FIX_ASPECT_RATIO);
+    double rpe = calibrateCamera(objectPoints,imagePoints1,Size(edges.size[0],edges.size[1]),
+                                 camera,distortion,rvecs,tvecs,
+                                 CV_CALIB_FIX_ASPECT_RATIO|
+                                 CV_CALIB_FIX_PRINCIPAL_POINT);
 
     if(found) imwrite("/home/ryan/chessboard.png",edges);
 
-    //cout << camera << endl;
+    cout << camera << endl;
     //cout << rpe << endl;
 
     return 0;
