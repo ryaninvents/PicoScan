f18067c05b716679c55c942306364b290fa6b185
Ryan Muller <ryan@ramuller.com>
2013-02-16 17:06:25 -0500

    ripped old code out of MainWindow by the roots

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,35 +6,18 @@
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow),
-    hardware(new HardwareManager),
-    screen(new ProjectionScreen),
-    manager(new ScanManager),
-    calib(new CalibrationDialog),
-    showingScreen(false),
-    binBit(0),
-    binInv(false),
-    binaryMode(true)
+    calib(new CalibrationDialog)
 {
     ui->setupUi(this);
 
     ui->debug->setPlainText(QString("ScanStudio started."));
 
-    debug("ScanManager created.");
-
     connect(&stdSettings,SIGNAL(accept()),this,SLOT(adjustCalStd()));
-
-    camSettings.setHardwareManager(hardware);
-    camSettings.setScanManager(manager);
-
-    stdSettings.setManager(manager);
-
-    hardware->refreshCameras();
 
 }
 
 MainWindow::~MainWindow()
 {
-    delete screen;
     delete ui;
     QCoreApplication::exit(0);
 
@@ -70,7 +53,6 @@ void MainWindow::showCameraSettings()
 {
     debug("Showing camera settings.");
     camSettings.show();
-    camSettings.setScanManager(manager);
     enableCalibrate();
 }
 
@@ -80,46 +62,8 @@ void MainWindow::showCalStdSettings()
     stdSettings.show();
 }
 
-void MainWindow::showProjectionScreen()
-{
-    if(!showingScreen){
-        screen->show();
-        showingScreen = true;
-
-        screen->projectBinary(0,false,255);
-        screen->projectOnDisplay(1);
-    }else if(binaryMode){
-        if(!binInv){
-            screen->projectBinary(binBit,binInv,255);
-            binInv = true;
-        }else{
-            if(binBit>=12){
-                binBit = 0;
-                binaryMode = false;
-                binBit++;
-                binInv = false;
-            }else{
-                screen->projectBinary(binBit,binInv,255);
-                binBit++;
-                binInv = false;
-            }
-        }
-    }
-    if(!binaryMode){
-        screen->projectSinusoid(8,binBit,255);
-        binBit++;
-        if(binBit>8){
-            binBit = 0;
-            binInv = false;
-            binaryMode = true;
-        }
-    }
-    manager->setScreen(screen);
-}
-
 void MainWindow::showCalibrationDialog()
 {
-    calib->setManager(manager);
     calib->show();
 }
 
@@ -130,13 +74,11 @@ void MainWindow::quitProgram()
 
 void MainWindow::adjustCalStd()
 {
-    manager->setStandard(stdSettings.getStandard());
     enableCalibrate();
 }
 
 void MainWindow::takeFrame()
 {
-    manager->takeBinaryFrame();
 }
 
 void MainWindow::closeEvent(QCloseEvent *)
--- mainwindow.h
+++ mainwindow.h
@@ -15,47 +15,58 @@
 namespace Ui {
 class MainWindow;
 }
-
+/// Represents the main Scan Studio window.
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
     
 public:
+    /// Create a new MainWindow.
     explicit MainWindow(QWidget *parent = 0);
+    /// Destroy the MainWindow.
     ~MainWindow();
 
 public slots:
+    /// Show the About dialog.
     void showAbout();
+    /// Show Scan Studio in fullscreen.
     void setFullScreen(bool fs);
+    /// Show the camera settings dialog.
     void showCameraSettings();
+    /// Show the calibration settings dialog.
     void showCalStdSettings();
-    void showProjectionScreen();
+    /// Show the calibration dialog.
     void showCalibrationDialog();
+    /// Quit Scan Studio.
     void quitProgram();
+    /// Adjust the calibration standard.
     void adjustCalStd();
+    /// Take a snapshot.
     void takeFrame();
+    /// Write debug info to the side panel.
     void debug(QString str);
+    /// Write debug info to the side panel.
     void debug(const char *str);
 
 protected:
+    /// Quit on close.
     void closeEvent(QCloseEvent *);
     
 private:
+    /// UI
     Ui::MainWindow *ui;
+    /// About dialog
     AboutDialog about;
+    /// Camera settings dialog
     CameraSettingsDialog camSettings;
+    /// Calibration standard dialog
     CalibrationStandardDialog stdSettings;
-    HardwareManager *hardware;
-    ScanManager *manager;
-    ProjectionScreen *screen;
+    /// Calibration dialog with the two views
     CalibrationDialog *calib;
 
+    /// Enable calibration
     void enableCalibrate();
 
-    bool showingScreen;
-    int binBit;
-    bool binInv;
-    bool binaryMode;
 };
 
 #endif // MAINWINDOW_H
